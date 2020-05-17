e91c6613cb0acd2bc4c086bbf1ba4a6f3597bff7
robert-rae-swanson <robert.rae.swanson@gmail.com>
2020-05-16 21:41:47 -0500

    Update Wikipedia_Coordinates_to_Google_Maps.js

--- src/Wikipedia_Coordinates_to_Google_Maps.js
+++ src/Wikipedia_Coordinates_to_Google_Maps.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Wikipedia Coordinates to Google Maps
-// @namespace    https://github.com/robert-rae-swanson
+// @namespace    https://github.com/robert-rae-swanson/userscripts/
 // @version      0.1
 // @description  Replace Wikipedia Coordinates that link to their GeoHack tool, with Google Maps
 // @author       Robert Swanson
+// @copyright    Robert Swanson
 // @match        *://*.wikipedia.org/*
 // @grant        none
-// @run-at document-end
+// @downloadURL  https://github.com/robert-rae-swanson/userscripts/blob/master/src/Wikipedia_Coordinates_to_Google_Maps.js
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
